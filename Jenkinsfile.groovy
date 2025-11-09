ac6970d3a54c90c23018da1a0b5acb40cf41161a
Pokrovszkaja Katerina <h773095@stud.u-szeged.hu>
2025-11-09 18:59:12 +0100

    Jenkins bekötése

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent any
+
+    tools {
+        nodejs 'NodeJS'
+    }
+
+    stages {
+        stage('Install Dependencies') {
+            steps {
+                dir('client') {  // If your React app is in a subfolder
+                    sh 'npm install'
+                }
+            }
+        }
+
+        stage('Lint') {
+            steps {
+                dir('client') {
+                    sh 'npm run lint || true'
+                }
+            }
+        }
+
+        stage('Build') {
+            steps {
+                dir('client') {
+                    sh 'npm run build'
+                }
+            }
+        }
+
+        stage('Test') {
+            steps {
+                dir('client') {
+                    sh 'npm test -- --watchAll=false --passWithNoTests'
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Build succeeded! 🎉'
+        }
+        failure {
+            echo 'Build failed! ❌'
+        }
+    }
+}
